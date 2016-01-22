fd5267ce5e2f86c14f505e5718b88b96199b1b8d
dast3095 <alex@alex-XPS-15-9530>
2016-01-21 18:18:34 -0700

    Forgot to add CMDHandlers.cpp and CMDHandlers.h

---
+++ include/servers/CMDHandlers.h
@@ -0,0 +1,25 @@
+/*
+ * CMDHandlers.h
+ *
+ *  Created on: Nov 19, 2015
+ *      Author: Alex St. Clair
+ */
+
+#include "core/MessageHandler.h"
+#include "core/ReturnMessage.h"
+#include "core/FSWPacket.h"
+#include "core/VariableTypeEnum.h"
+
+class CMDSwitchProtocolHandler : public Phoenix::Core::MessageHandler
+{
+public:
+        /*! \brief Handle a request for a protocol switch
+         *
+         *  \param packet FSWPacket containing the message.
+         *  \return ReturnMessage that serves as a response.
+         */
+		Phoenix::Core::ReturnMessage * Handle(const Phoenix::Core::FSWPacket & packet);
+private:
+		const static uint32 numParams = 2;
+		static uint32 enumArray[numParams];
+};
---
+++ src/servers/CMDHandlers.cpp
@@ -0,0 +1,72 @@
+/*
+ * CMDHandlers.cpp
+ *
+ *  Created on: Nov 19, 2015
+ *      Author: Alex St. Clair
+ */
+
+#include "servers/CMDHandlers.h"
+#include "servers/CMDStdTasks.h"
+#include "servers/CMDServer.h"
+#include "servers/DispatchStdTasks.h"
+
+#include "core/CommandMessage.h"
+#include "core/ReturnMessage.h"
+#include "core/ErrorMessage.h"
+#include "core/DataMessage.h"
+#include "core/Singleton.h"
+#include "core/Factory.h"
+#include "core/Dispatcher.h"
+#include "core/StdTypes.h"
+#include "core/ModeManager.h"
+
+using namespace std;
+using namespace Phoenix::Core;
+using namespace Phoenix::Servers;
+
+// define necessary parameters
+// extract them from message
+// perform error checking on parameters
+// subsystem_acp_protocol[subsystem] = protocol;
+
+// outputArray[0]
+
+uint32 CMDSwitchProtocolHandler::enumArray[] = {VAR_TYPE_ENUM_UNSIGNED_INT, VAR_TYPE_ENUM_UNSIGNED_INT};
+
+ReturnMessage * CMDSwitchProtocolHandler::Handle(const FSWPacket & packet)
+{
+	void * outputArray[numParams] = {NULL};
+	if(!ExtractParameters(packet, enumArray, numParams, outputArray))
+	{
+		ErrorMessage err(CMD_ACP_SWITCH_FAILURE);
+		ReturnMessage * ret = new ReturnMessage(&err, false);
+		return ret;
+	}
+
+	uint32 subsystem = * (uint32 *) outputArray[0];
+	uint32 protocol = * (uint32 *) outputArray[1];
+
+
+	// Error Checking
+	if((subsystem < HARDWARE_LOCATION_MIN) || (subsystem >= HARDWARE_LOCATION_MAX))
+	{
+		ErrorMessage err(CMD_ACP_SWITCH_BAD_SUBSYS);
+		ReturnMessage * ret = new ReturnMessage(&err, false);
+		return ret;
+	}
+	if((protocol < ACP_PROTOCOL_MIN) || (protocol >= ACP_PROTOCOL_MAX))
+	{
+		ErrorMessage err(CMD_ACP_SWITCH_BAD_PROTOCOL);
+		ReturnMessage * ret = new ReturnMessage(&err, false);
+		return ret;
+	}
+
+
+	// Get CMDServer Instance, and change protocol for given subsystem
+	CMDServer * cmdServer = dynamic_cast<CMDServer *> (Factory::GetInstance(CMD_SERVER_SINGLETON));
+	cmdServer->subsystem_acp_protocol[subsystem] = protocol;
+
+	DataMessage msg(CMD_ACP_SWITCH_SUCCESS);
+	ReturnMessage * ret = new ReturnMessage(&msg, true);
+	return ret;
+}
