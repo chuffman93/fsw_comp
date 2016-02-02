21d8a86be975341b4ff0a1195e6b2a013f349078
dast3095 <alex@alex-XPS-15-9530>
2016-02-02 16:42:03 -0700

    Fixing Git problems due to COSGC password reset

--- test/testFileHandler1001.cpp
+++ test/testFileHandler1001.cpp
@@ -17,12 +17,5 @@ TEST(TestFileHandler, exist) {
 
 	ASSERT_FALSE(fileHandler->FileExists("noooope.txt"));
 	ASSERT_TRUE(fileHandler->FileExists("CMD_List_1.dat")); //this file should be in home directory
-}
-
-TEST(TestFileHandler, truDat){
-
-	Factory::GetInstance(FILE_HANDLER_SINGLETON);
-	FileHandler * fileHandler = dynamic_cast<FileHandler *> (Factory::GetInstance(FILE_HANDLER_SINGLETON));
-
-	ASSERT_TRUE(fileHandler->FileExistsInTruDat("CMD_List_1.dat"));
+	EXPECT_EQ(fileHandler->fileSize("CMD_List_1.dat"),0);
 }
