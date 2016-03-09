94ee368d5a96dbe9e7af3f7503938d4a26b3e659
dast3095 <dast3095@colorado.edu>
2016-03-08 17:22:53 -0700

    Continued CDHServer architecture. Added CDHHandlers.cpp and *.h

---
+++ include/servers/CDHStdTasks.h
@@ -0,0 +1,24 @@
+/*
+ * CDHStdTasks.h
+ *
+ *  Created on: Mar 8, 2016
+ *      Author: alex
+ */
+
+#ifndef CDHSTDTASKS_H_
+#define CDHSTDTASKS_H_
+
+#include "core/CommandMessage.h"
+#include "core/ReturnMessage.h"
+#include "core/StdTypes.h"
+
+namespace Phoenix
+{
+	namespace Servers
+	{
+		Phoenix::Core::ReturnMessage * CDHUsage(void);
+	}
+}
+
+
+#endif /* CDHSTDTASKS_H_ */
