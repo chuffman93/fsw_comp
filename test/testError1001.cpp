b6a5b364f9ef3a98d01fd4a935476f04186526ac
Cy Parker <cy.parker@colorado.edu>
2016-05-20 12:24:58 -0600

    Revert "Finished testing of error logging"
    This reverts commit c990650c41062e45e10b3750e75fc121b37a0630.
    
    Conflicts:
    
    	src/core/Dispatcher.cpp
    	src/core/FSWPacket.cpp

--- test/testError1001.cpp
+++ test/testError1001.cpp
@@ -15,6 +15,9 @@
 #include "stdlib.h"
 #include <stdio.h>
 #include <pthread.h>
+#include "util/itoa.h"
+#include <sys/sysinfo.h>
+#include <sys/statvfs.h>
 
 #include "servers/ErrorOctopus.h"
 #include "servers/ErrorStdTasks.h"
@@ -25,26 +28,26 @@ using namespace rel_ops;
 using namespace Phoenix::Core;
 using namespace Phoenix::Servers;
 
-// Necessary to run the error server
 void * taskRunERR(void * params)
 {
 	ErrorOctopus * errServer = dynamic_cast<ErrorOctopus *> (Factory::GetInstance(ERR_SERVER_SINGLETON));
 	ModeManager * modeManager = dynamic_cast<ModeManager *> (Factory::GetInstance(MODE_MANAGER_SINGLETON));
+	//WatchdogManager * wdm = dynamic_cast<WatchdogManager *> (Factory::GetInstance(WATCHDOG_MANAGER_SINGLETON));
 
 	modeManager->Attach(*errServer);
+	//wdm->Kick();
 
 	errServer->RegisterHandlers();
 	errServer->SubsystemLoop();
 	pthread_exit(NULL);
 }
 
-// Successful
 TEST(TestErrorHandling, testLog)
 {
 	// Grab necessary instances, set mode ---------------------------------------------------------------------------------------
 	FileHandler * fileHandler = dynamic_cast<FileHandler *>(Factory::GetInstance(MODE_MANAGER_SINGLETON));
 	ModeManager * modeManager = dynamic_cast<ModeManager *>(Factory::GetInstance(MODE_MANAGER_SINGLETON));
-	modeManager->SetMode(MODE_ERROR, LOCATION_ID_INVALID);
+	modeManager->SetMode(MODE_ACCESS, LOCATION_ID_INVALID);
 
 
 	// Release the error octopus ------------------------------------------------------------------------------------------------
@@ -63,14 +66,10 @@ TEST(TestErrorHandling, testLog)
 	// Send and process error message for each server ----------------------------------------------------------------------------
 	int errors[] = {CDH_CPU_USAGE_FAILURE, CMD_ACP_SWITCH_FAILURE, EPS_HS_FAILURE, ACS_HS_FAILURE, GPS_HS_FAILURE, COM_HS_FAILURE, PLD_HS_FAILURE, SCH_BUILD_SCHEDULE_FAILURE, THM_HS_FAILURE};
 	int servers[] = {SERVER_LOCATION_CDH, SERVER_LOCATION_CMD, SERVER_LOCATION_EPS, SERVER_LOCATION_ACS, SERVER_LOCATION_GPS, SERVER_LOCATION_COM, SERVER_LOCATION_PLD, SERVER_LOCATION_SCH, SERVER_LOCATION_THM};
-	for(int i=0; i<9; i++)
-	{
+	for(int i=0; i<9; i++){
 		ErrorMessage err(errors[i]);
 		ReturnMessage * ret = new ReturnMessage(&err, false);
 		MessageProcess(servers[i], ret);
-		usleep(1000000);
 	}
-
-	// Note: To verify test success, must check filehandler output!
 
 }
