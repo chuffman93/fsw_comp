634c6368790f597b1dafd1bde3a67e7002921893
Cy Parker <cy.parker@colorado.edu>
2017-01-09 13:08:04 -0700

    refactored FMGServer tests

--- test/testFMGServer.cpp
+++ test/testFMGServer.cpp
@@ -15,50 +15,16 @@ TEST(DISABLED_TestFMGServer, testPacketDestinations){
 	WatchdogManager::StartServer(fmgServer, 0,	true);
 
 	for (int iter = 0; iter < 10; iter++) {
-		FilePacket myPacket2;
-		myPacket2.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket2.dest = DESTINATION_HST;
-		fmgServer->FileQueue.push(myPacket2);
-
-		FilePacket myPacket3;
-		myPacket3.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket3.dest = DESTINATION_MOD;
-		fmgServer->FileQueue.push(myPacket3);
-
-		FilePacket myPacket4;
-		myPacket4.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket4.dest = DESTINATION_SWP;
-		fmgServer->FileQueue.push(myPacket4);
-
-		FilePacket myPacket5;
-		myPacket5.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket5.dest = DESTINATION_ERR;
-		fmgServer->FileQueue.push(myPacket5);
-
-		FilePacket myPacket6;
-		myPacket6.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket6.dest = DESTINATION_DGN;
-		fmgServer->FileQueue.push(myPacket6);
-
-		FilePacket myPacket7;
-		myPacket7.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket7.dest = DESTINATION_GEN;
-		fmgServer->FileQueue.push(myPacket7);
-
-		FilePacket myPacket8;
-		myPacket8.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket8.dest = DESTINATION_FSS;
-		fmgServer->FileQueue.push(myPacket8);
-
-		FilePacket myPacket9;
-		myPacket9.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket9.dest = DESTINATION_SSS;
-		fmgServer->FileQueue.push(myPacket9);
-
-		FilePacket myPacket10;
-		myPacket10.buffer = "12345678901234567890123456789012345678901234567890";
-		myPacket10.dest = DESTINATION_RAD;
-		fmgServer->FileQueue.push(myPacket10);
+		string str = "12345678901234567890123456789012345678901234567890";
+		fmgServer->Log(DESTINATION_HST, str);
+		fmgServer->Log(DESTINATION_MOD, str);
+		fmgServer->Log(DESTINATION_SWP, str);
+		fmgServer->Log(DESTINATION_ERR, str);
+		fmgServer->Log(DESTINATION_DGN, str);
+		fmgServer->Log(DESTINATION_GEN, str);
+		fmgServer->Log(DESTINATION_FSS, str);
+		fmgServer->Log(DESTINATION_SSS, str);
+		fmgServer->Log(DESTINATION_RAD, str);
 	}
 
 	while (fmgServer->FileQueue.size() > 0) {
@@ -78,6 +44,10 @@ TEST(DISABLED_TestFMGServer, testNoDestination){
 		fmgServer->FileQueue.push(myPacket1);
 	}
 
+	while (fmgServer->FileQueue.size() > 0) {
+			usleep(100000);
+	}
+
 	// Need to manually verify that no files were created
 }
 
@@ -89,6 +59,10 @@ TEST(DISABLED_TestFMGServer, testLongMessage){
 		myPacket11.buffer = "this is a very long message. There is lots and lots and lots of data here! Oh no! What am I possibly going to do with all this data?? Who knows? But one thing is for sure... There is a hell of a lot of data here. Jeez, it just keeps on coming doesn't it. Yessir. Just keeps on coming. Why are you still reading this buffer anyway? Don't you have better things to be doing than reading a buffer??? Okay, I'm done.";
 		myPacket11.dest = DESTINATION_GEN;
 		fmgServer->FileQueue.push(myPacket11);
+	}
+
+	while (fmgServer->FileQueue.size() > 0) {
+			usleep(100000);
 	}
 
 	// Need to manually verify that files exist
