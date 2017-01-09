f978ce61e119e38e3c5eb5fbe8c2fba723a12ebd
Cy Parker <cy.parker@colorado.edu>
2017-01-09 11:36:17 -0700

    added clean_filesystem script

---
+++ scripts/clean_filesystem.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+#######################################
+# clean_filesystem.sh
+# Last Modified: Jack Dinkel 01/09/17
+#
+# Removes all data files inside the CDH
+#   filesystem
+#######################################
+
+# All directories on /SD_2
+TLM_DIRECTORIES="
+GEN
+HST
+MOD
+SWP
+ERR
+DGN
+FSS
+SSS
+"
+
+# All directories on /SD_3
+SCI_DIRECTORIES="
+RAD
+"
+
+for dir in $TLM_DIRECTORIES
+do
+  for file in `ls -p /SD_2/$dir | grep -v /`
+    do
+      rm /SD_2/$dir/$file
+    done
+
+  for file in `ls -p /SD_2/$dir/CUR | grep -v /`
+    do
+      rm /SD_2/$dir/CUR/$file
+    done
+done
+
+for dir in $SCI_DIRECTORIES
+do
+  for file in `ls -p /SD_3/$dir | grep -v /`
+    do
+      rm /SD_3/$dir/$file
+    done
+
+  for file in `ls -p /SD_3/$dir/CUR | grep -v /`
+    do
+      rm /SD_3/$dir/CUR/$file
+    done
+done
