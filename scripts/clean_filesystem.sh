f974d66eace0e0e5c8b863c19d4f01d1663ece38
Cy Parker <cy.parker@colorado.edu>
2017-01-09 13:03:25 -0700

    improved clean_filesystem.sh

--- scripts/clean_filesystem.sh
+++ scripts/clean_filesystem.sh
@@ -27,6 +27,7 @@ RAD
 
 for dir in $TLM_DIRECTORIES
 do
+  echo "Cleanning /SD_2/$dir"
   for file in `ls -p /SD_2/$dir | grep -v /`
     do
       rm /SD_2/$dir/$file
@@ -40,6 +41,7 @@ done
 
 for dir in $SCI_DIRECTORIES
 do
+  echo "Cleanning /SD_3/$dir"
   for file in `ls -p /SD_3/$dir | grep -v /`
     do
       rm /SD_3/$dir/$file
