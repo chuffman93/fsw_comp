063e134a8eb5770c3d7858f737aac3fad96dd840
Cy Parker <cy.parker@colorado.edu>
2017-02-08 15:43:10 -0700

    Fixed some autocoding bugs

--- TLM/auto_coder/py/cpp.py
+++ TLM/auto_coder/py/cpp.py
@@ -12,10 +12,19 @@ class CPP_AutoCoder(TLM_AutoCoder):
   def __exit__(self, exc_type, exc_value, traceback):
     TLM_AutoCoder.__exit__(self, exc_type, exc_value, traceback)
 
+  def using_namespace(self):
+    print "using namespace std;"
+    print "using namespace AllStar::Core;"
+    print
+
+  def fmg_server(self):
+    print "FMGServer * fmgServer = dynamic_cast<FMGServer *> (Factory::GetInstance(FMG_SERVER_SINGLETON));"
+    print
+
   def print_func(self):
     print "void %s%s(%s) {" % (self.gen_func_name, self.name, self.args)
     print "  uint8_t buf[%s];" % (self.buf_size)
-    print "  ser.Serialize ser(buf, %s);" % (self.buf_size)
+    print "  Serialize ser(buf, %s);" % (self.buf_size)
     print "  ser.serializeUInt16(%s);" % (self.ID)
     for i in range(self.list_len):
         print "  ser.serialize%s(%s);" % (self.type_list[i], self.arg_list[i])
@@ -37,7 +46,11 @@ class CPP_AutoCoder(TLM_AutoCoder):
   def autocode(self):
     self.header()
     self.include()
+    self.using_namespace()
+    self.namespace_start()
+    self.fmg_server()
     self.autocode_all_tlm()
+    self.namespace_end()
 
 
 
--- include/util/TLM.h
+++ include/util/TLM.h
@@ -6,8 +6,12 @@
  *
  ******************************************************/
 
+namespace AllStar{
+namespace Servers{
+
 #include <stdint.h>
 #include "util/serialize.h"
+#include "servers/FMGServer.h"
 
 void TLM_STARTUP_ERROR(void);
 void TLM_SERVERS_CREATED(void);
@@ -16,3 +20,6 @@ void TLM_SPI_STATS(void);
 void TLM_SAMPLE_TEST_2(void);
 void TLM_SAMPLE_TEST_3(void);
 void TLM_CREATING_ACPPACKET(int32 arg0);
+}
+}
+
