506211a88dfcad30fd05465972d1d9cfbe51e784
Jack Dinkel <johnpauldinkel@gmail.com>
2017-02-08 14:36:23 -0700

    added new tlm files

--- TLM/auto_coder/autocode.sh
+++ TLM/auto_coder/autocode.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python py/cpp.py ../dict.txt > TLM.cpp
-python py/hpp.py ../dict.txt > TLM.h
+python py/cpp.py ../dict.txt > ../../src/util/TLM.cpp
+python py/hpp.py ../dict.txt > ../../include/util/TLM.h
