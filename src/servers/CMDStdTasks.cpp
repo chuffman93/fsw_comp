1a081f3ebe9a075274ffd98a10c21f2eac0e2e89
Cy Parker <cy.parker@colorado.edu>
2017-11-08 19:02:23 -0700

    Did mtu the right way this time

--- src/servers/CMDStdTasks.cpp
+++ src/servers/CMDStdTasks.cpp
@@ -77,8 +77,9 @@ void uftpSetup(void) {
 		system("kill $(ps aux | grep '[s]l0' | awk '{print $2}')"); // kill uftp process if still initialized to avoid attempt at re-bind
 	}
 	logger->Info("CMDStdTasks: UFTP bind not found and formed");
-	system("slattach -L -m 256 -s 115200 -p slip /dev/ttyS2 &");
-	system("ifconfig sl0 1.1.1.1 pointopoint 1.1.1.2 multicast up");
+
+	system("slattach -L -m -s 115200 -p slip /dev/ttyS2 &");
+	system("ifconfig sl0 1.1.1.1 pointopoint 1.1.1.2 mtu 256 multicast up");
 
 
 
