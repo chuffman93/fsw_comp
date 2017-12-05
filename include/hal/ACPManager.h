ebc5feecf1c96faa9dbb3ea473eaafd03a4abe5e
Robert Belter <robbotorigami@gmail.com>
2017-12-04 20:09:39 -0700

    Wrote ACPManager.h

---
+++ include/hal/ACPManager.h
@@ -0,0 +1,44 @@
+/*
+ * ACPManager.h
+ *
+ *  Created on: Dec 4, 2017
+ *      Author: cyborg9
+ */
+
+#ifndef ACPMANAGER_H_
+#define ACPMANAGER_H_
+
+#include "hal/BusManager.h"
+#include "hal/SPIManager.h"
+#include "hal/InterruptManager.h"
+#include "util/ACPPacket.h"
+
+//! Contains the ids for the corrisponding spi and interrupt devices
+struct ACPDevice{
+	int spiid;
+	int intid;
+};
+
+/*!
+ * Manages a SPI bus and interrupt lines.
+ * Handles transactions with the assosiated subsystems.
+ */
+class ACPManager: public BusManager<ACPDevice>{
+public:
+	ACPManager(SPIManager& spiman, InterruptManager& intman);
+	~ACPManager();
+
+	void attachDevice(int spiid, int intid);
+
+	bool transaction(ACPPacket& packet, ACPPacket& ret);
+
+private:
+	//! Reference to the SPIManager to be used
+	SPIManager& spiman;
+	//! Reference to the InterruptManager to be used
+	InterruptManager& intman;
+};
+
+
+
+#endif /* ACPMANAGER_H_ */
