80eb7d4502d8490c45a2afc424bd78daa4cd3615
Robert Belter <robbotorigami@gmail.com>
2017-12-29 09:12:31 -0700

    Added MockI2C and HotSwap interface

---
+++ include/interfaces/HotSwapInterface.h
@@ -0,0 +1,38 @@
+/*
+ * HotSwapInterface.h
+ *
+ *  Created on: Dec 29, 2017
+ *      Author: cyborg9
+ */
+
+#ifndef HOTSWAPINTERFACE_H_
+#define HOTSWAPINTERFACE_H_
+
+#include "hal/I2CManager.h"
+#include "util/Logger.h"
+
+struct HotSwapData{
+	float current;
+	float voltage;
+};
+
+class HotSwapInterface{
+public:
+	HotSwapInterface(I2CManager& i2cman, int id, float senseR, std::string instance);
+	~HotSwapInterface();
+
+	HotSwapData getData();
+	void configure();
+
+private:
+	//! The reference to the i2c manager
+	I2CManager& i2cman;
+	//! The id of the i2c device
+	int id;
+	//! The value of the sense resistor to use for current conversion
+	float senseR;
+	//! Tags for logging
+	LogTags tags;
+};
+
+#endif /* HOTSWAPINTERFACE_H_ */
---
+++ src/interfaces/HotSwapInterface.cpp
@@ -0,0 +1,45 @@
+/*
+ * HotSwapInterface.cpp
+ *
+ *  Created on: Dec 29, 2017
+ *      Author: cyborg9
+ */
+
+#include "interfaces/HotSwapInterface.h"
+
+/*!
+ * Creates a hot swap interface to the given i2c device
+ * \param i2cman the i2c bus
+ * \param id the id of the device
+ * \param instance A unique string for the instance
+ */
+HotSwapInterface::HotSwapInterface(I2CManager& i2cman, int id, float senseR, std::string instance)
+:i2cman(i2cman), id(id), senseR(senseR)
+{
+	tags += LogTag("Name", "HotSwapInterface");
+	tags += LogTag("Instance", instance);
+}
+
+HotSwapInterface::~HotSwapInterface(){}
+
+HotSwapData HotSwapInterface::getData(){
+	HotSwapData retval;
+	std::vector<uint8_t> data = i2cman.readRaw(id, 3);
+	uint16_t voltRaw = ((uint16_t)data[0]<<4) | (0x0F & (data[2]>>4));
+	uint16_t currRaw = ((uint16_t)data[1]<<4) | (0x0F & data[2]);
+
+	retval.voltage = 26.35 * ((float)voltRaw/4096); //Volts
+	retval.current = 0.10584 * ((float)currRaw/4096) / senseR; //Current
+
+	Logger::Stream(LEVEL_INFO, tags) << "Read: " << retval.voltage << "V " << retval.current << "A";
+	return retval;
+}
+
+void HotSwapInterface::configure(){
+	std::vector<uint8_t> data;
+	data.push_back(0x05);
+	Logger::log(LEVEL_INFO, tags, "Configuring HotSwap");
+	i2cman.writeRaw(id, data);
+}
+
+
