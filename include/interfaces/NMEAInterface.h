28bb1781519c17cd316b721acbfa162d79236d80
Robert Belter <robbotorigami@gmail.com>
2017-12-30 02:22:11 -0700

    Added header for NMEAInterface

---
+++ include/interfaces/NMEAInterface.h
@@ -0,0 +1,15 @@
+/*
+ * NMEAInterface.h
+ *
+ *  Created on: Dec 30, 2017
+ *      Author: cyborg9
+ */
+
+#ifndef NMEAINTERFACE_H_
+#define NMEAINTERFACE_H_
+
+
+
+
+
+#endif /* NMEAINTERFACE_H_ */
