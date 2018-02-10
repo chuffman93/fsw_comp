115d9cf4f7eab06862bc9f247ef9ffa0940dfd3c
chuffman93 <cohu8717@colorado.edu>
2018-02-09 19:24:02 -0700

    Deleted useless junk

--- DITL/Debug/src/Lock.d
+++
@@ -1,4 +0,0 @@
-src/Lock.d: ../src/Lock.cpp \
- /home/alpa3119/eclipse-workspace/DITL/include/Lock.h
-
-/home/alpa3119/eclipse-workspace/DITL/include/Lock.h:
--- DITL/Debug/src/Logger.d
+++
@@ -1,7 +0,0 @@
-src/Logger.d: ../src/Logger.cpp \
- /home/alpa3119/eclipse-workspace/DITL/include/Logger.h \
- /home/alpa3119/eclipse-workspace/DITL/include/Lock.h
-
-/home/alpa3119/eclipse-workspace/DITL/include/Logger.h:
-
-/home/alpa3119/eclipse-workspace/DITL/include/Lock.h:
