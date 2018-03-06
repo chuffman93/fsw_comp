f3ac166414a64b2108b9925fba40c2f54d75c3f0
Robert Belter <cy.parker@colorado.edu>
2018-03-05 18:01:21 -0700

    fixing the ditl issue

--- DITL/Debug/src/TimeKeeper.d
+++
@@ -1,4 +0,0 @@
-src/TimeKeeper.d: ../src/TimeKeeper.cpp \
- /home/alpa3119/eclipse-workspace/DITL/include/TimeKeeper.h
-
-/home/alpa3119/eclipse-workspace/DITL/include/TimeKeeper.h:
--- DITL/src/test/unittest.d
+++
@@ -1,4 +0,0 @@
-src/test/unittest.d: ../src/test/unittest.cpp \
- /home/fsw/Robert/cdh-fsw/include/test/catch.hpp
-
-/home/fsw/Robert/cdh-fsw/include/test/catch.hpp:
